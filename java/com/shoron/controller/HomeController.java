d1e6dc41ddafab92d2d54b04c7afd6cc93a10f98
shaekhhasanshoron <shaekhhasanshoron@gmail.com>
2018-04-11 10:12:22 +0600

    Adding date field to add todo page

--- java/com/shoron/controller/HomeController.java
+++ java/com/shoron/controller/HomeController.java
@@ -1,16 +1,20 @@
 package com.shoron.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.core.SpringVersion;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +31,12 @@ public class HomeController {
 
 	@Autowired
 	TodoService todoService;
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder){
+		SimpleDateFormat dateFormat=new SimpleDateFormat("dd/MM/yyyy");
+		binder.registerCustomEditor(Date.class,new CustomDateEditor(dateFormat, false));
+	}
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String indexPage() {
@@ -62,8 +72,7 @@ public class HomeController {
 		model.clear();
 		model.addAttribute("todo",todo);
 		return "add-update-todo";
-	}
-	
+	}	
 	
 	@RequestMapping(value = "/updatetodo", method = RequestMethod.POST)
 	public String updateTodo(ModelMap model,@Valid Todo todo, BindingResult result) {
